4d93c30cba4cb7dc1e59215d5dce45230ab3edf8
XingYueChenFu <lixinzhe@stu.scu.edu.cn>
2024-12-14 02:10:02 +0800

    修改member对于查询多个用户的方法

--- backend/gym_manager/routes/member.py
+++ backend/gym_manager/routes/member.py
@@ -24,15 +24,15 @@ def staff_regist_member():
     if request.method == 'OPTIONS':
         return jsonify({'msg': 'CORS preflight response'}), 200
     md = hashlib.md5()
-    md.update(request.args.get('phone_number').encode('utf-8'))
+    md.update(request.json.get('phone_number').encode('utf-8'))
 
     member = Member()
-    member.phone_number = request.args.get('phone_number')
-    member.nickname = request.args.get('nickname')
-    member.realname = request.args.get('realname')
-    member.id_card = request.args.get('id_card')
-    member.student_card = request.args.get('student_card')
-    member.gender = request.args.get('gender')
+    member.phone_number = request.json.get('phone_number')
+    member.nickname = request.json.get('nickname')
+    member.realname = request.json.get('realname')
+    member.id_card = request.json.get('id_card')
+    member.student_card = request.json.get('student_card')
+    member.gender = request.json.get('gender')
     member.password = md.hexdigest()
 
     if db.session.query(Member).filter_by(phone_number=member.phone_number).first() is not None:
@@ -71,14 +71,13 @@ def staff_get_members():
     if request.method == 'OPTIONS':
         return jsonify({'msg': 'CORS preflight response'}), 200
 
-    # print(request.args)
-    print(request.json)
     # 获取查询参数
-    page = request.args.get('page', default=1, type=int)
-    per_page = request.args.get('per_page', default=10, type=int)
+    page = request.json.get('page', 1)
+    per_page = request.json.get('per_page', 10)
     # 可选的过滤条件
-    nickname = request.args.get('nickname', default=None, type=str)
-    phone_number = request.args.get('phone_number', default=None, type=str)
+    nickname = request.json.get('nickname', None)
+    phone_number = request.json.get('phone_number', None)
+    print(request.json.get('nickname', None))
 
     # 构建查询
     query = Member.query
