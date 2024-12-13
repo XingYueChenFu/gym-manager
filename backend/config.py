5ab32ebca2d9ce84168d4682aa23e2f305066d2e
yll <1823710298@qq.com>
2024-12-13 19:58:32 +0800

    格式化

--- backend/config.py
+++ backend/config.py
@@ -1,40 +1,45 @@
-import os  
+import os
 
-basedir = os.path.abspath(os.path.dirname(__file__)) # 绝对路径
+basedir = os.path.abspath(os.path.dirname(__file__))  # 绝对路径
 
-# ===== 配置基类 ===== 
+# ===== 配置基类 =====
+
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or r'mysql+pymysql://admin:HZtj4oZLe*MiA7y3@101.91.146.59/gym_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or r'mysql+pymysql://admin:HZtj4oZLe*MiA7y3@101.91.146.59/gym_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # 关闭对模型修改的监控
-    SECRET_KEY='kdjklfjkd87384hjdhjh' # 用于加密 session 的密钥 可以换为其他字符串
+    SECRET_KEY = 'kdjklfjkd87384hjdhjh'  # 用于加密 session 的密钥 可以换为其他字符串
     DEBUG = False
     JSON_AS_ASCII = False
-    
+
     # 自定义参数
-    RESET_DATABASE = False # 重置数据库 (不)
+    RESET_DATABASE = False  # 重置数据库 (不)
     HOST_IPV4 = '127.0.0.1'
     PORT = 5000
-    
 
-# ===== 开发环境配置 ===== 
+
+# ===== 开发环境配置 =====
 class DevelopmentConfig(Config):  # 开发模式
     # RESET_DATABASE = True # 重置数据库
     DEBUG = True
 
-# ===== 测试环境配置 ===== 
+# ===== 测试环境配置 =====
+
+
 class TestingConfig(Config):
     pass
 
-# ===== 生产环境配置 ===== 
+# ===== 生产环境配置 =====
+
+
 class ProductionConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
-config = { # 配置选项字典
+config = {  # 配置选项字典
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
     'default': DevelopmentConfig
-}  
-
-    
+}
