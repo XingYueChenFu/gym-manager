44e7358e3a4f7224c749798b509e251b2db8a722
XingYueChenFu <lixinzhe@stu.scu.edu.cn>
2024-12-14 14:02:37 +0800

    添加查询expiration_time

--- backend/gym_manager/routes/expiration_time.py
+++ backend/gym_manager/routes/expiration_time.py
@@ -14,3 +14,17 @@ import hashlib
 import uuid
 from datetime import datetime
 
+@staff_bp.route('/query/expiration_time/<int:id>', methods=['POST', 'OPTIONS'])
+# @login_required
+# @permission(1)
+def staff_get_expiration_time_by_id(id):
+    if request.method == 'OPTIONS':
+        return jsonify({'msg': 'CORS preflight response'}), 200
+    expiration_time = ExpirationTime.query.filter_by(member_id=id).first()
+    if expiration_time is not None:
+        data = {'deadline': expiration_time.deadline.strftime('%Y-%m-%d %H:%M:%S')}
+        return jsonify({'msg': '成功', 'code': 200, 'data': data})
+    else:
+        return jsonify({'msg': '用户信息为空', 'code': 2008})
+        
+    return jsonify({'msg': '失败', 'code': 2004})
