2ac5b345f9595bddb4b3dabda75bbe26f4200f4d
yll <1823710298@qq.com>
2024-12-17 04:00:21 +0800

    完成了deal的增和改，接口未匹配

--- frontend/src/views/ActivityView.vue
+++ frontend/src/views/ActivityView.vue
@@ -40,9 +40,6 @@
 							<router-link to="/member/detail" class="nav-link">Details</router-link>
 						</li>
 						<li class="nav-item">
-							<router-link to="/member/add" class="nav-link">Add new member</router-link>
-						</li>
-						<li class="nav-item">
 							<router-link to="/member/record" class="nav-link">Record</router-link>
 						</li>
 						<li class="nav-item">
@@ -51,97 +48,6 @@
 					</ul>
 				</div>
 			</li>
-
-			<li class="nav-item">
-				<a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false"
-					aria-controls="form-elements">
-					<i class="icon-columns menu-icon"></i>
-					<span class="menu-title">Form elements</span>
-					<i class="menu-arrow"></i>
-				</a>
-				<div class="collapse" id="form-elements">
-					<ul class="nav flex-column sub-menu">
-						<li class="nav-item"><a class="nav-link" href="../../pages/forms/basic_elements.html">Basic
-								Elements</a></li>
-					</ul>
-				</div>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false"
-					aria-controls="charts">
-					<i class="icon-bar-graph menu-icon"></i>
-					<span class="menu-title">Charts</span>
-					<i class="menu-arrow"></i>
-				</a>
-				<div class="collapse" id="charts">
-					<ul class="nav flex-column sub-menu">
-						<li class="nav-item"> <a class="nav-link" href="../../pages/charts/chartjs.html">ChartJs</a>
-						</li>
-					</ul>
-				</div>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" data-bs-toggle="collapse" href="#tables" aria-expanded="false"
-					aria-controls="tables">
-					<i class="icon-grid-2 menu-icon"></i>
-					<span class="menu-title">Tables</span>
-					<i class="menu-arrow"></i>
-				</a>
-				<div class="collapse" id="tables">
-					<ul class="nav flex-column sub-menu">
-						<li class="nav-item"> <a class="nav-link" href="../../pages/tables/basic-table.html">Basic
-								table</a>
-						</li>
-					</ul>
-				</div>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" data-bs-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
-					<i class="icon-contract menu-icon"></i>
-					<span class="menu-title">Icons</span>
-					<i class="menu-arrow"></i>
-				</a>
-				<div class="collapse" id="icons">
-					<ul class="nav flex-column sub-menu">
-						<li class="nav-item"> <a class="nav-link" href="../../pages/icons/mdi.html">Mdi icons</a></li>
-					</ul>
-				</div>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-					<i class="icon-head menu-icon"></i>
-					<span class="menu-title">User Pages</span>
-					<i class="menu-arrow"></i>
-				</a>
-				<div class="collapse" id="auth">
-					<ul class="nav flex-column sub-menu">
-						<li class="nav-item"> <a class="nav-link" href="../../pages/samples/login.html"> Login </a></li>
-						<li class="nav-item"> <a class="nav-link" href="../../pages/samples/register.html"> Register
-							</a></li>
-					</ul>
-				</div>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" data-bs-toggle="collapse" href="#error" aria-expanded="false" aria-controls="error">
-					<i class="icon-ban menu-icon"></i>
-					<span class="menu-title">Error pages</span>
-					<i class="menu-arrow"></i>
-				</a>
-				<div class="collapse" id="error">
-					<ul class="nav flex-column sub-menu">
-						<li class="nav-item"> <a class="nav-link" href="../../pages/samples/error-404.html"> 404 </a>
-						</li>
-						<li class="nav-item"> <a class="nav-link" href="../../pages/samples/error-500.html"> 500 </a>
-						</li>
-					</ul>
-				</div>
-			</li>
-			<!-- <li class="nav-item">
-				<a class="nav-link" href="../../../docs/documentation.html">
-				<i class="icon-paper menu-icon"></i>
-				<span class="menu-title">Documentation</span>
-				</a>
-			</li> -->
 		</ul>
 	</nav>
 
@@ -152,9 +58,72 @@
         <div class="col-lg-12 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-                    <h4 class="card-title">Activity Table</h4>
-                    <p class="card-description"> Add class <code>.table</code>
-                    </p>
+					<div class="row">
+						<h4 class="col-4 card-title">Activity Table</h4>
+						<!-- <div class="col-8 justify-content-end d-flex">
+							<button type="button" class="btn btn-primary mb-2" @click="handleSubmit">Submit</button>
+						</div> -->
+						<div class="justify-content-end d-flex col-8">
+							<button class="btn btn-light" @click="handleCancel">Cancel</button>
+							<span style="width: 2vh;"></span>
+							<button type="button" class="btn btn-primary" @click="handleSubmit">{{bt_name}}</button>
+						</div>
+					</div>
+					 <div class="card "> <!-- v-if="isVisible" -->
+						<p class="card-description"> Click one, Modify it; or Add New Activity. <code> {{ warnning }} </code> </p>
+						<div class="row pt-2">
+							<div class="col-2">
+								<div class="form-group">
+									<label>Activity</label>
+									<input type="text" class="form-control form-control-sm" placeholder="Activity Name" v-model="formData.activity_name">
+								</div>
+							</div>
+							<div class="col-2">
+								<div class="form-group">
+									<label>Type</label>
+									<select class="form-select form-select-lg" v-model="formData.recharge_type">
+										<option value="time">Time</option>
+										<option value="count">Count</option>
+									</select>
+								</div>
+							</div>
+							<div class="col-3">
+								<div class="form-group" v-if="isVisible">
+									<label>Day</label>
+									<input type="text" class="form-control form-control-sm" placeholder="obtain Day" v-model="formData.recharge_day">
+								</div>
+								<div class="row" v-if="!isVisible">
+									<div class="form-group col-md-6">
+										<label>Count</label>
+										<input type="text" class="form-control form-control-sm" placeholder="obtain Count" v-model="formData.recharge_count">
+									</div>
+									<div class="form-group col-md-6" >
+										<label>Lifespan</label>
+										<input type="text" class="form-control form-control-sm" placeholder="period of validity" v-model="formData.lifespan">
+									</div>
+								</div>
+							</div>
+							<div class="col-2">
+								<div class="form-group">
+									<label>Amount</label>
+									<input type="text" class="form-control form-control-sm" placeholder="Plan Amount" v-model="formData.amount">
+								</div>
+							</div>
+							<div class="col-3">
+								<div class="row">
+									<div class="form-group col-md-6">
+										<label>Start Time</label>
+										<input type="text" class="form-control form-control-sm" placeholder="xxxx-xx-xx" v-model="formData.start_time">
+									</div>
+									<div class="form-group col-md-6" >
+										<label>End Time</label>
+										<input type="text" class="form-control form-control-sm" placeholder="year-month-day" v-model="formData.end_time">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
                     <div class="table-responsive">
                       <table class="table">
                         <thead>
@@ -166,7 +135,7 @@
                           </tr>
                         </thead>
                         <tbody>
-						  	<tr v-for="(opt, index) in opts" :key="index" @click="memberRecharge(index)">
+						  	<tr v-for="(opt, index) in opts" :key="index" @click="cheakDetail(index)">
 								<td>{{ opt.activity_name }}</td>
 								<td>{{ opt.plan_id }}</td>
 								<td>{{ opt.obtain }}</td>
@@ -186,21 +155,11 @@
 
 
 <script lang='ts' setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import axios from "axios";
-import { useRouter } from 'vue-router';
-import { useMemberStore } from '../stores/member';
-
-// 获取 router 实例
-const router = useRouter();
-const memberStore = useMemberStore();
-const memberId = memberStore.$state.memberInfo.member_id;
-// console.log(memberStore.$state.memberInfo.member_id);  // 要展示的 member_id
 
 // 定义表格项的接口
 interface opt {
-//   id: string;        // 充值的时间
-//   : string;    // 截止的时间 or none
     'activity_id': number,
 	'plan_id': number,
 	'activity_name': string,
@@ -215,7 +174,7 @@ interface opt {
 	'obtain': string,
 }
 const opts = ref<opt[]>([]);
-
+// 获取活动详情
 const fetchDealForm = async () => {
 	try {
 		// 发送请求
@@ -248,33 +207,88 @@ const fetchDealForm = async () => {
 };
 fetchDealForm()
 
-
-const memberRecharge = async (index:number) => {
-	try {
-		// 发送请求
-		const response = await axios.post(
-			'http://localhost:5000/staff/recharge',
-			{
-				"member_id": memberId,
-				"activity_id": opts.value[index].activity_id,
-				"plan_id": opts.value[index].plan_id,
-			},
-			{
-				headers: {
-				'Content-Type': 'application/json',
-				},
-			});
-		// 响应处理
-		if (response.data.code === 200) {
-			alert("充值成功，即将返回充值记录页面");
-			router.push('/member/record');
-		} else {
-			console.log("充值失败");
-			return;
-		}
-	} catch (error) {
-		console.error("充值失败:", error);
-		alert("充值失败，请稍后再试！");
+// 响应式表单数据
+const formData = ref({
+	'activity_name': '',
+	'start_time': '',
+	'end_time': '',
+	'recharge_type': 'time',
+	'recharge_count': null,
+	'lifespan': null,
+	'recharge_day': null,
+	'amount': null,
+	'activity_remark': null,
+});
+// 修改的活动索引
+const idx = ref<number>(-1);
+// 修改活动的警告
+const warnning = ref<string>('');
+const bt_name = ref<string>('Add');
+// const isVisible = ref<boolean>(false);
+const isVisible = computed(() => {
+  if (formData.value.recharge_type === 'time') {
+    return true;
+  } else {
+    return false;
+  }
+});
+// 提交按钮的处理逻辑
+const handleSubmit = async () => {
+  try {
+    const payload = { ...formData.value }; // 提交前的表单数据
+	  // 发送 POST 请求
+	let path = '';
+	if (idx.value === -1) {  // 新增
+		path = 'http://localhost:5000/staff/add/deal';
+	  }
+	else {  // 修改
+		path = 'http://localhost:5000/staff/modify/deal';
 	}
+
+    const response = await axios.post(
+      path,
+      payload,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    console.log('Response:', response.data);
+    // 请求成功后的处理逻辑
+    if (response.data.code === 200) {
+		alert('Activity added successfully!');
+		handleCancel();
+      	fetchDealForm();
+    }
+    else {
+      alert('Failed to add Activity.');
+    }
+  } catch (error) {
+    // 请求失败的处理逻辑
+    console.error('Error while adding Activity:', error);
+    alert('Failed to add Activity.');
+  }
+};
+const cheakDetail = (index: number) => {
+	Object.assign(formData.value, opts.value[index]);
+	idx.value = index;
+	warnning.value = 'Check And Modify The Activity ' + `${index + 1}` + ' !';
+	bt_name.value = 'Modify';
+};
+const handleCancel = () => {
+	idx.value = -1;
+	warnning.value = '';
+	bt_name.value = 'Add';
+	Object.assign(formData.value, {
+		'activity_name': '',
+		'start_time': '',
+		'end_time': '',
+		'recharge_count': null,
+		'lifespan': null,
+		'recharge_day': null,
+		'amount': null,
+		'activity_remark': null,
+	});
 };
 </script>
